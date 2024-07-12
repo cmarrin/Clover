79f2a4ea9efec8484d71cbee69762e8d2b3e2940
Chris Marrin <cmarrin@apple.com>
2024-07-12 15:55:52 -0700

    WIP - Added code to do interp

---
+++ Memory.h
@@ -0,0 +1,12 @@
+//
+//  Memory.h
+//  LucidVM
+//
+//  Created by Chris Marrin on 7/7/24.
+//
+
+#ifndef Memory_h
+#define Memory_h
+
+
+#endif /* Memory_h */
--- mac/interp.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  interp.cpp
-//  LucidVM
-//
-//  Created by Chris Marrin on 5/24/24.
-//
-
-#include <stdio.h>
