247c384dee8fc74769a454df656f973257aeb578
Chris Marrin <cmarrin@apple.com>
2024-07-22 15:46:41 -0700

    Changed addLocal to return Symbol

--- Struct.h
+++ Struct.h
@@ -41,16 +41,28 @@ public:
         return _structs.back();
     }
 
-    bool addLocal(const std::string& name, Type type, uint8_t size, bool ptr)
+    Function* addFunction(const std::string& name, Type returnType)
+    {
+        SymbolPtr symbol = findLocal(name);
+        if (symbol) {
+            return nullptr;
+        }
+        
+        Function* function = Module::addFunction(name, returnType);
+        _locals.push_back(std::make_shared<Symbol>(function));
+        return function;
+    }
+
+    SymbolPtr addLocal(const std::string& name, Type type, uint8_t size, bool ptr)
     {
         // Check for duplicates
         SymbolPtr symbol = findLocal(name);
         if (symbol) {
-            return false;
+            return nullptr;
         }
         _locals.push_back(std::make_shared<Symbol>(name, type, size, ptr));
         _localSize += size;
-        return true;
+        return _locals.back();
     }
 
     SymbolPtr findLocal(const std::string& s)
