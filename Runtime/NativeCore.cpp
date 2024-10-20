0c4a408618ea7a6388e9f9b47b323e3ec78c11b8
Chris Marrin <chris@marrin.com>
2024-10-20 10:46:12 -0700

    Got rid of animate and frand from core
    Animate is now a Clover function. To do frand, scale the floating point numbers to make them integers and then scale back.

--- Runtime/NativeCore.cpp
+++ Runtime/NativeCore.cpp
@@ -26,7 +26,6 @@ NativeCore::createModule()
                                                                            { "v", Type::UInt8, false, 1, 1 },
                                                                            { "n", Type::UInt32, false, 1, 1 }});
     coreModule->addNativeFunction("irand", uint16_t(Id::RandomInt), Type::Int32, {{ "min", Type::Int32, false, 1, 1 }, { "max", Type::Int32, false, 1, 1 }});
-    coreModule->addNativeFunction("frand", uint16_t(Id::RandomFloat), Type::Float, {{ "min", Type::Float, false, 1, 1 }, { "max", Type::Float, false, 1, 1 }});
     coreModule->addNativeFunction("imin", uint16_t(Id::MinInt), Type::Int32, {{ "a", Type::Int32, false, 1, 1 }, { "b", Type::Int32, false, 1, 1 }});
     coreModule->addNativeFunction("imax", uint16_t(Id::MaxInt), Type::Int32, {{ "a", Type::Int32, false, 1, 1 }, { "b", Type::Int32, false, 1, 1 }});
     coreModule->addNativeFunction("fmin", uint16_t(Id::MinFloat), Type::Float, {{ "a", Type::Float, false, 1, 1 }, { "b", Type::Float, false, 1, 1 }});
@@ -36,7 +35,6 @@ NativeCore::createModule()
     coreModule->addNativeFunction("argint16", uint16_t(Id::ArgInt16), Type::Int16, { });
     coreModule->addNativeFunction("argint32", uint16_t(Id::ArgInt32), Type::Int32, { });
     coreModule->addNativeFunction("argFloat", uint16_t(Id::ArgFloat), Type::Float, { });
-    coreModule->addNativeFunction("animate", uint16_t(Id::Animate), Type::Int8, { { "p", Type::None, true, 1, 1 } });
     return coreModule;
 }
 #endif
@@ -64,12 +62,6 @@ NativeCore::callNative(uint16_t id, InterpreterBase* interp)
             int32_t a = interp->memMgr()->getArg(0, 4);
             int32_t b = interp->memMgr()->getArg(4, 4);
             interp->setReturnValue(random(a, b));
-            break;
-        }
-        case Id::RandomFloat: {
-            float a = intToFloat(interp->memMgr()->getArg(0, 4));
-            float b = intToFloat(interp->memMgr()->getArg(4, 4));
-            interp->setReturnValue(floatToInt(float(random(int32_t(a * 1000), int32_t(b * 1000))) / 1000));
             break;
         }
         case Id::MemSet: {
@@ -125,43 +117,6 @@ NativeCore::callNative(uint16_t id, InterpreterBase* interp)
         case Id::ArgFloat:
             interp->setReturnValue(interp->topLevelArgs()->arg(4));
             break;
-        case Id::Animate: {
-            // Passes in a pointer to a struct with cur, inc, min and max
-            // values, all floats. Perform one animation iteration of cur
-            // by adding inc to it. When it hits min or max, negate inc
-            // to go the other direction next time. Return -1 if we just
-            // finished going down, or 1 if we just finished going up.
-            // Otherwise return 0.
-            float cur, inc, min, max;
-            AddrNativeType addr = interp->memMgr()->getArg(0, AddrSize);
-            cur = intToFloat(interp->memMgr()->getAbs(addr, 4));
-            inc = intToFloat(interp->memMgr()->getAbs(addr + 4, 4));
-            min = intToFloat(interp->memMgr()->getAbs(addr + 8, 4));
-            max = intToFloat(interp->memMgr()->getAbs(addr + 12, 4));
-
-            cur += inc;
-            interp->memMgr()->setAbs(addr, floatToInt(cur), OpSize::flt);
-            interp->setReturnValue(0);
-
-            if (0 < inc) {
-                if (cur >= max) {
-                    cur = max;
-                    inc = -inc;
-                    interp->memMgr()->setAbs(addr, floatToInt(cur), OpSize::flt);
-                    interp->memMgr()->setAbs(addr + 4, floatToInt(inc), OpSize::flt);
-                    interp->setReturnValue(1);
-                }
-            } else {
-                if (cur <= min) {
-                    cur = min;
-                    inc = -inc;
-                    interp->memMgr()->setAbs(addr, floatToInt(cur), OpSize::flt);
-                    interp->memMgr()->setAbs(addr + 4, floatToInt(inc), OpSize::flt);
-                    interp->setReturnValue(-1);
-                }
-            }
-            break;
-        }
     }
 }
 #endif
